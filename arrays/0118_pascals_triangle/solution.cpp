62db76dfbb19bf3b9bb77fe39a2f3cd7deb0850b
teja15162 <chintala.teja15162@gmail.com>
2026-08-11 16:35:43 +0530

    Pascal's Triangle solution

--- arrays/0118_pascals_triangle/solution.cpp
+++ arrays/0118_pascals_triangle/solution.cpp
@@ -1,32 +1,29 @@
-class Solution {
+class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> ans;
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> triangle;
 
-        if(numRows == 1) {
-            vector<int> temp = {1};
-            ans.push_back(temp);
-            return ans;
-        }
+        // if numRows = 1, then just return [1]
+        if(numRows == 1) {
+            vector<int> temp = {1};
+            triangle.push_back(temp);
+            return triangle;
+        }
 
-        for(int i=0;i<=numRows;i++) {
-            vector<int> temp;
-            long long ans = 1;
-            
-            for(int j=0;j<=i;j++) {
-                if(j == 0) {
-                    temp.push_back(ans);
-                    continue;
-                }
+        for(int i=0;i<numRows;i++) {
+            vector<int> temp;
+            long long ans = 1;
+            R
+            for(int j=0;j<=i;j++) {
+                if(j == 0) {
+                    temp.push_back(ans);
+                    continue;
+                }
 
-                ans *= (i - j);
-                ans /= j;
-                temp.push_back(ans);
-            }
+                ans *= (i - j +1);
+                ans /= j;
+                temp.push_back(ans);
+            }
 
-            ans.push_back(temp);
-        }
-
-        return ans;
-    }
-};
+            triangle.push_back(temp);
+        }
